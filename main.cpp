b06a67b78a76718ebe60ebc494e53d51b4bf790f
Rainforest-Sun <104972938+Rainforest-Sun@users.noreply.github.com>
2022-07-01 14:09:47 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 
 int main()
 {
-    freopen("C:\\Users\\rainforest\\Desktop\\testcases\\array_test1.data", "r", stdin);
-    freopen("C:\\Users\\rainforest\\Desktop\\out.txt", "w", stdout);
     read_order();
     run_order();
     printf("%u\n",reg[10]&255u);
